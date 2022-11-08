c3f46a805aac7dadd8a32625f31a7dc26e96b3b3
PyeongYeonCho <vuddus526@naver.com>
2022-11-08 20:11:08 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(long n) {
+        String str = String.valueOf(n);
+        int[] answer = new int[str.length()];
+        
+        for(int i=0; i<str.length(); i++){
+            answer[i] = str.charAt(str.length() - i - 1)-'0';
+        }
+        return answer;
+    }
+}
