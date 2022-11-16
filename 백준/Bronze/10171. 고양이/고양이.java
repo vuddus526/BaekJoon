79593989fe9316277089dfb25f32a1115b983c88
PyeongYeonCho <vuddus526@naver.com>
2022-11-16 19:14:11 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+
+        // int A = scanner.nextInt();
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
