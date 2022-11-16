bd0aa5a6b127e97b57f201b379e25ce8494a3159
PyeongYeonCho <vuddus526@naver.com>
2022-11-16 19:16:25 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+
+        // int A = scanner.nextInt();
+        String s = "         ,r'\"7\n" + 	// \", \n 이 제어문자다.
+            "r`-_   ,'  ,/\n" + 		// \n 이 제어문자다.
+            " \\. \". L_r'\n" + 		// \\, \", \n 이 제어문자다.
+            "   `~\\/\n" + 			// \\, \n 이 제어문자다.
+            "      |\n" + 			// \n 이 제어문자다.
+            "      |";
+
+        System.out.print(s);
+    }
+}
