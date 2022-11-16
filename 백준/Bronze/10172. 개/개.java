dcacd26ad3b794e46199360968d27ea3f975a48f
PyeongYeonCho <vuddus526@naver.com>
2022-11-16 19:15:06 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+
+        // int A = scanner.nextInt();
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
