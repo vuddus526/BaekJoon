a76e31631e76b6dfc181d1202f3b84aa7977cefd
PyeongYeonCho <vuddus526@naver.com>
2023-03-23 22:48:17 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 232 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// 그냥 입력값을 그대로 출력하면 된다
+
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}
